f847887780a721766303ac2bc44bfd135e3e97b0
GarnaceAprilDianne23 <garnaceaprildianne@gmail.com>
2025-03-18 19:42:15 +0800

    Add files via upload

---
+++ CS1C_GARNACE_A_PERF1.js
@@ -0,0 +1,13 @@
+// Difference of let, const, and var in JavaScript
+
+// var
+var exampleVar = "I am a var variable";
+exampleVar = "I can be reassigned";
+var exampleVar = "I can be redeclared";
+
+// let
+let exampleLet = "I am a let variable";
+exampleLet = "I can be reassigned";
+
+// const
+const exampleConst = "I am a const variable";
